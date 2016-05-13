6442febd6a0de0419b05e119e75890d8b1432574
Ericka <ves_VS@hotmail.com>
2016-05-13 11:23:53 -0400

    Practice#1

---
+++ Ericka/print_practice_rb.rb
@@ -0,0 +1,15 @@
+print "first line"
+#in the first line up we are using "print"
+puts "second line"
+#in the second line up we are using "puts"
+print "third line"
+puts "hello"
+print "world"
+puts "BDT"
+print "jalasoft"
+=begin
+	
+that is a script with the objetive to 
+see the difference between "print"
+and "puts" for the BDT class 	
+=end
